0fff9aab23d9cd4d7130223c628468414a30c187
la_victoire <oghuvbuvictory@gmail.com>
2025-02-08 00:59:44 +0100

    modified:   app/startup/[id]/page.tsx 	modified:   sanity/lib/queries.ts

--- app/startup/[id]/page.tsx
+++ app/startup/[id]/page.tsx
@@ -17,7 +17,7 @@ export const experimental_ppr = true;
 const page = async ({params}: {params: Promise<{id: string}>}) => {
   const id = (await params).id;
 
-  const [post , { select: editorPosts}] = await Promise.all([
+  const [post , editorPosts] = await Promise.all([
    client.fetch(STARTUPS_BY_ID_QUERY, {id}),
    client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: "editor-s-picks"})
   ])
